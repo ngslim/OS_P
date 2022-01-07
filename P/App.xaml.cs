8521ae45cde69e09e49edb39acfb3f193e8c6a23
Nguyễn Sĩ Liêm <ngggslim@gmail.com>
2022-01-07 13:02:29 +0700

    Add project files.

---
+++ P/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P
+{
+    public static class Constant
+    {
+        public const String APP_DIRECTORY = @"C:\Users\Public\Parental Control\";
+        public const String SCREENSHOT_DIRECTORY = @"C:\Users\Public\Parental Control\Screenshots\";
+    }
+    public partial class App : Application
+    {
+    }
+}
---
+++ P/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace P
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            start();
+        }
+
+        private void refreshImageList()
+
+        {
+            String searchFolder = Constant.SCREENSHOT_DIRECTORY;
+            var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" };
+            var files = GetFilesFrom(searchFolder, filters, false);
+            foreach (var file in files)
+            {
+                imageList.Items.Add(file);
+            }
+        }
+
+        private async void start()
+        {
+            while(true)
+            {
+                refreshImageList();
+                await Task.Delay(60000);
+            }
+        }
+
+        private static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+    }
+}
